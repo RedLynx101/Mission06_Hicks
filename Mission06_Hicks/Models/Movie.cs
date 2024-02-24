5566726adc1cf092f1c1adfa28bc30e8280482f8
RedLynx101 <NoahHicks101@gmail.com>
2024-02-23 17:39:01 -0700

    The most significant changes involve modifications to the `Movie` model and the `NMovie.cshtml` file. The `Movie` model has been updated to change the requirements for the `Category` and `Director` properties, add a range validation for the `Year` property, and introduce a new required property `CopiedToPlex`. The `NMovie.cshtml` file has been altered to reflect these changes, with the `required` attribute removed from the `Category` and `Director` input fields, a `min` attribute added to the `Year` input field, and a new checkbox input field for `CopiedToPlex`.
    List of changes:
    
    1. The `Category` and `Director` properties in the `Movie` model are no longer required and can be null. This change allows for more flexibility when creating or updating movie records. (Code Change: Movie Model)
    
    2. A range validation has been added to the `Year` property in the `Movie` model, limiting it to the years 1888 to 2100. This ensures that only valid years are entered for a movie. (Code Change: Movie Model)
    
    3. The `Edited` property in the `Movie` model is now required. This ensures that the edited status of a movie is always specified. (Code Change: Movie Model)
    
    4. A new required property `CopiedToPlex` has been added to the `Movie` model. This allows tracking of whether a movie has been copied to Plex. (Code Change: Movie Model)
    
    5. In the `NMovie.cshtml` file, the `required` attribute has been removed from the `Category` and `Director` input fields. This reflects the changes in the `Movie` model where these properties are no longer required. (Code Change: NMovie.cshtml)
    
    6. The `Year` input field in the `NMovie.cshtml` file now has a `min` attribute set to 1888. This enforces the range validation added to the `Year` property in the `Movie` model. (Code Change: NMovie.cshtml)
    
    7. A new checkbox input field for `CopiedToPlex` has been added to the `NMovie.cshtml` file. This allows users to specify whether a movie has been copied to Plex when creating or updating a movie record. (Code Change: NMovie.cshtml)

--- Mission06_Hicks/Models/Movie.cs
+++ Mission06_Hicks/Models/Movie.cs
@@ -8,21 +8,26 @@ namespace Mission06_LastName.Models
         public int Id { get; set; }
 
         [Required]
-        public string Category { get; set; }
+        public string? Category { get; set; }
 
         [Required]
         public string Title { get; set; }
 
         [Required]
+        [Range(1888, 2100)]
         public int Year { get; set; }
 
         [Required]
-        public string Director { get; set; }
+        public string? Director { get; set; }
 
         [Required]
         public string Rating { get; set; }
-
-        public bool Edited { get; set; }
+        
+        [Required]
+        public bool Edited { get; set; } // Empty returns false, so it will return some value regardless of user intent
+        
+        [Required]
+        public bool CopiedToPlex { get; set; }
 
         public string? LentTo { get; set; }
 
